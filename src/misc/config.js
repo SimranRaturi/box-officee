393e510f3da06f8ee3c22b4234ee3758c5d97c56
simmyyy <simmy123@>
2021-05-30 22:26:17 +0530

    added loading and error message to the show page

--- src/misc/config.js
+++ src/misc/config.js
@@ -5,7 +5,10 @@ const API_BASE_URL = 'https://api.tvmaze.com';
  export async function apiGet(queryString){
 
 const response = await  fetch(`${API_BASE_URL}${queryString}`).then(r =>
-   r.json() )
+   r.json()
+    );
+
+  //  throw new Error('oops');
   return response;
 
 }
